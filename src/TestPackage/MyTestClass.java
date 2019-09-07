5b22eb815a38dd6b085ac72adbdd7283b337e9b0
HOME <HOME@HOME-PC>
2019-09-07 11:21:41 +0530

    This is First Commit

---
+++ src/TestPackage/MyTestClass.java
@@ -0,0 +1,11 @@
+package TestPackage;
+
+public class MyTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
