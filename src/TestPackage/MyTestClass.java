f4e96467f5b24850121cae298563e3fff3362085
HOME <HOME@HOME-PC>
2019-09-07 11:29:50 +0530

    this is second commit

--- src/TestPackage/MyTestClass.java
+++ src/TestPackage/MyTestClass.java
@@ -5,6 +5,12 @@ public class MyTestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		int a=1;
+		int b=2;
+		int c=a+b;
+		System.out.println(c);
+		
 
 	}
 
